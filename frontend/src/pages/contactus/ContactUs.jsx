e01fb21573eab08da720b7081616366ee3de0dbb
Amna-parambat <amnu2005@gmail.com>
2026-06-22 16:48:24 +0530

    Added Contact Us page

---
+++ frontend/src/pages/contactus/ContactUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ContactHero from "./components/ContactHero";
+import QuickAnswers from "./components/QuickAnswers";
+import MovementCTA from "./components/MovementCTA";
+
+const ContactUs = () => {
+  return (
+    <>
+      <ContactHero />
+      <QuickAnswers />
+      <MovementCTA />
+    </>
+  );
+};
+
+export default ContactUs;
